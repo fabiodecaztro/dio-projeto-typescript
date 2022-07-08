1628020a1401664885400cc2b43fcf71133cc3f2
fabiodecaztro <89656058+fabiodecaztro@users.noreply.github.com>
2022-07-08 18:16:11 -0300

    Exercício 1

---
+++ app.js
@@ -0,0 +1,9 @@
+"use strict";
+const exercise1 = () => {
+    console.log("Exercício 1\n");
+    const employee1 = {
+        code: 1234,
+        name: "John",
+    };
+    console.log(`Name: ${employee1.name} - Code: ${employee1.code}.`);
+};
---
+++ app.ts
@@ -0,0 +1,14 @@
+const exercise1 = () => {
+    console.log("Exercício 1\n");
+    interface Employee {
+        code: number;
+        name: string;
+    }
+
+    const employee1: Employee = {
+        code: 1234,
+        name: "John",
+    };
+
+    console.log(`Name: ${employee1.name} - Code: ${employee1.code}.`);
+};
